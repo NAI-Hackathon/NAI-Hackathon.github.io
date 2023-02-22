557538ec8ca68afe512cc180e431237dcc0087c4
Rosie-the-Brilliant <101292338+Rosie-the-Brilliant@users.noreply.github.com>
2023-02-22 17:48:26 -0500

    fixed navBar/responsive!!!!

--- helper.js
+++ helper.js
@@ -1,16 +1,16 @@
 //============== Pic Slides =============//
 //Question
-let qIndex = 1;
-showSlides(qIndex);
+var qInd = 1;
+showSlides(qInd);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(qIndex += n);
+  showSlides(qInd += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(qIndex = n);
+  showSlides(qInd = n);
 }
 
 
@@ -18,16 +18,26 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {qIndex = 1}
-  if (n < 1) {qIndex = slides.length}
+  if (n > slides.length) {qInd = 1}
+  if (n < 1) {qInd = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[qIndex-1].style.display = "block";
-  dots[qIndex-1].className += " active";
+  slides[qInd-1].style.display = "block";
+  dots[qInd-1].className += " active";
 }
 
 //============================================================//
+//============== Nav Dropdown Small screens =============//
+function navDrop(){
+  var x = document.getElementById("links");
+  if (x.className == '') {
+    x.className = "down";
+  }
+  else {
+    x.className = "";
+  }
+}
